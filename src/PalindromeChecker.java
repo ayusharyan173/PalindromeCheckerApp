b75e2ed0cdebf9621e91964b9c02547a22a4512a
Ayush Aryan <aa7435@srmist.edu.in>
2026-02-23 10:46:41 +0530

    UC5: Stack-Based Palindrome Checker

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -7,14 +7,12 @@ public class PalindromeChecker {
 
         Stack<Character> stack = new Stack<>();
 
-        // Push characters into stack using for-each loop
         for (char c : input.toCharArray()) {
             stack.push(c);
         }
 
         boolean isPalindrome = true;
 
-        // Pop characters and compare
         for (char c : input.toCharArray()) {
             if (c != stack.pop()) {
                 isPalindrome = false;
@@ -22,7 +20,6 @@ public class PalindromeChecker {
             }
         }
 
-        // Print result
         if (isPalindrome) {
             System.out.println(input + " is a Palindrome.");
         } else {
