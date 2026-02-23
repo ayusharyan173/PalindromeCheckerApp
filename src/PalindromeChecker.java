210e2d943c033689aad8a517e53e4a21aa3e4445
ayusharyan173 <aa7435@srmist.edu.in>
2026-02-23 10:58:28 +0530

    UC7: Dequeue Based Palindrome Checker

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,30 +1,33 @@
-import java.util.Stack;
-import java.util.Queue;
+import java.util.Deque;
 import java.util.LinkedList;
 
 public class PalindromeChecker {
-    public static void main(String[] args) {
-
-        String input = "civic";
-
-        Stack<Character> stack = new Stack<>();
-        Queue<Character> queue = new LinkedList<>();
+    public static boolean isPalindrome(String input) {
+        Deque<Character> deque = new LinkedList<>();
 
         for (char c : input.toCharArray()) {
-            stack.push(c);   // LIFO
-            queue.add(c);    // FIFO
+            deque.addLast(c);
         }
 
         boolean isPalindrome = true;
 
-        while (!queue.isEmpty()) {
-            if (stack.pop() != queue.remove()) {
+        while (deque.size() > 1) {
+            char front = deque.removeFirst();
+            char rear = deque.removeLast();
+
+            if (front != rear) {
                 isPalindrome = false;
                 break;
             }
         }
 
-        if (isPalindrome) {
+        return isPalindrome;
+    }
+
+    public static void main(String[] args) {
+        String input = "refer";
+
+        if (isPalindrome(input)) {
             System.out.println(input + " is a Palindrome.");
         } else {
             System.out.println(input + " is NOT a Palindrome.");
